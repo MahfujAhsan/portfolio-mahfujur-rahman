5197baee2e0b0ab673bd87a3529c28ac2bcc293b
MahfujAhsan <ahsanmahfuj@gmail.com>
2022-07-18 13:12:28 +0600

    navbar updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,13 +6,13 @@ import { NavHashLink as Link } from 'react-router-hash-link';
 
 const Navbar = () => {
     return (
-        <div className='container w-75 py-4 shadow-lg'>
+        <div className='container w-75 py-4 shadow-lg sticky-top'>
             <nav className='text-center d-flex justify-content-around align-items-center'>
-                <Link smooth to="/"><FontAwesomeIcon className='text-white' icon={faHomeLg} size="2x" title='Home'/></Link>
-                <Link smooth to="/#about"><FontAwesomeIcon className='text-white' icon={faUser} size="2x" title='About'/></Link>
-                <Link smooth to="/#myProjects"><FontAwesomeIcon className='text-white' icon={faDisplay} size="2x" title='Projects'/></Link>
-                <Link smooth to="/#contact"><FontAwesomeIcon className='text-white' icon={faIdCard} size="2x" title='Contact' /></Link>
-                <Link smooth to="/blog"><FontAwesomeIcon title='Blogs' className='text-white' icon={faBlog} size="2x" /></Link>
+                <Link smooth to="/"><FontAwesomeIcon className='text-danger' icon={faHomeLg} size="2x" title='Home'/></Link>
+                <Link smooth to="/#about"><FontAwesomeIcon className='text-danger' icon={faUser} size="2x" title='About'/></Link>
+                <Link smooth to="/#myProjects"><FontAwesomeIcon className='text-danger' icon={faDisplay} size="2x" title='Projects'/></Link>
+                <Link smooth to="/#contact"><FontAwesomeIcon className='text-danger' icon={faIdCard} size="2x" title='Contact' /></Link>
+                <Link smooth to="/blog"><FontAwesomeIcon title='Blogs' className='text-danger' icon={faBlog} size="2x" /></Link>
             </nav>
         </div>
     );
